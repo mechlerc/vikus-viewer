0241b919f7edfc7792062d552d32978acd201fd6
mechlerc <mechler.cintia@gmail.com>
2022-03-05 23:21:01 -0300

    clonando WCMA

--- js/timeline.js
+++ js/timeline.js
@@ -1,156 +1,159 @@
 function Timeline() {
-  var fontScaleYear = d3.scale
-    .linear()
-    .domain([1, 9])
-    .range([9, 20])
-    .clamp(true);
 
-  var timelineFontScale = d3.scale
-    .linear()
-    .domain([40, 8])
-    .range([2, 10])
-    .clamp(true);
+    var fontScaleYear = d3.scale.linear()
+        .domain([1, 9])
+        .range([9, 20])
+        .clamp(true)
 
-  var timelineScale = d3.scale
-    .threshold()
-    .domain([4.5, 10, 20]) // change none>small from 3
-    .range(["none", "small", "middle", "large"]);
+    var timelineFontScale = d3.scale.linear()
+        .domain([40, 8])
+        .range([2, 10])
+        .clamp(true)
 
-  var timelineHover = false;
-  var container;
-  var timeDomain;
-  var fontSize = 1;
-  var disabled = false;
+    var timelineScale = d3.scale.threshold()
+        .domain([4.5, 10, 20]) // change none>small from 3
+        .range(["none", "small", "middle", "large"])
 
-  function timeline() {}
+    var timelineHover = false;
+    var container
+    var timeDomain
+    var fontSize = 1
+    var disabled = false
 
-  timeline.init = function (_timeDomain) {
-    timeDomain = _timeDomain;
-    container = d3.select(".viz").append("div").classed("timeline", true);
-    container.style(
-      "transform",
-      "translate(" + 0 + "px," + (canvas.height() - 30) + "px)"
-    );
-  };
+    function timeline() {}
 
-  timeline.rescale = function (scale) {
-    timeDomain.forEach(function (d) {
-      d.x = canvas.x(d.key);
-    });
-    fontSize = timelineFontScale(scale);
-  };
+    timeline.init = function (_timeDomain) {
+        timeDomain = _timeDomain
+        container = d3.select(".viz").append("div").classed("timeline", true)
+        container.style("transform", "translate(" + 0 + "px," + (canvas.height() - 30) + "px)");
+    }
 
-  timeline.setDisabled = function (d) {
-    disabled = d;
-    container.style("display", disabled ? "none" : "block");
-  };
+    timeline.rescale = function (scale) {
+        timeDomain.forEach(function (d) {
+            d.x = canvas.x(d.key);
+        });
+        fontSize = timelineFontScale(scale)
+    }
 
-  timeline.update = function (x1, x2, scale, translate) {
-    if (disabled) return;
+    timeline.setDisabled = function (d) {
+        disabled = d
+        container.style("display", disabled ? "none" : "block")
+    }
 
-    timeDomain.forEach(function (d) {
-      d.pos = (d.x - x1) * scale;
-      d.visible =
-        d.pos > -canvas.rangeBand() * scale && d.pos < canvas.width() + 100;
-    });
+    timeline.update = function (x1, x2, scale, translate) {
+        if (disabled) return
 
-    var timeY =
-      canvas.height() * scale -
-      -1 * translate[1] -
-      canvas.rangeBandImage() * scale;
+        timeDomain.forEach(function (d) {
+            d.pos = ((d.x - x1) * scale);
+            d.visible = (d.pos > (-canvas.rangeBand() * scale) && d.pos < canvas.width() + 100);
+        })
 
-    container
-      .attr("class", "timeline " + timelineScale(scale * (fontSize / 2)))
-      .style("font-size", function () {
-        return fontSize * scale + "px";
-      })
-      .style("transform", "translate3d(" + 0 + "px," + timeY + "px, 0px)");
+        var timeY = ((canvas.height()) * scale - (-1 * translate[1]) - canvas.rangeBandImage() * scale);
 
-    var select = container.selectAll(".container").data(timeDomain);
+        container
+            .attr("class", "timeline " + timelineScale(scale * (fontSize / 2)))
+            .style("font-size", function () {
+                return fontSize * scale + "px";
+            })
+            .style("transform", "translate3d(" + 0 + "px," + timeY + "px, 0px)");
 
-    var enter = select
-      .enter()
-      .append("div")
-      .classed("container", true)
-      .on("mouseenter", function (d) {
-        timelineHover = true;
-        canvas.zoom.center(null);
-        // canvas.selectedImage() = null;
-      })
-      .on("mouseleave", function (d) {
-        timelineHover = false;
-      });
+        var select = container.selectAll(".container")
+            .data(timeDomain)
 
-    enter
-      .append("div")
-      .classed("year", true)
-      .classed("decade", function(d){
-        // if timeline scale is none and key % 10 is 0 then true
-        return (d.key % 10 === 0)
-    })
-      .text(function (d) {
-        return d.key;
-      });
+        var enter = select
+            .enter()
+            .append("div")
+            .classed("container", true)
+            .on("mouseenter", function (d) {
+                timelineHover = true;
+                canvas.zoom.center(null);
+                // canvas.selectedImage() = null;
+            })
+            .on("mouseleave", function (d) {
+                timelineHover = false;
+            })
 
-    var e = enter
-      .append("div")
-      .classed("entries", true)
-      .selectAll(".entry")
-      .data(function (d) {
-        return d.values;
-      })
-      .enter()
-      .append("div")
-      .classed("entry", true);
+        enter.append("div")
+            .classed("year", true)
+            .classed("decade", function(d){
+                // if timeline scale is none and key % 10 is 0 then true
+                return (d.key % 10 === 0)
+            })
+            .text(function (d) {
+                return d.key;
+            })
 
-    e.append("div")
-      .classed("small", true)
-      .append("div")
-      .classed("title", true)
-      .text(function (d) {
-        return d.titel;
-      });
+        var e = enter
+            .append("div")
+            .classed("entries", true)
+            .selectAll(".entry")
+            .data(function (d) {
+                return d.values;
+            })
+            .enter()
+            .append("div")
+            .classed("entry", true)
 
-    var m = e.append("div").classed("middle", true);
+        e
+            .append("div")
+            .classed("small", true)
+            .append("div")
+            .classed("title", true)
+            .text(function (d) {
+                return d.titel;
+            })
 
-    m.append("div")
-      .classed("title", true)
-      .text(function (d) {
-        return d.titel;
-      });
+        var m = e
+            .append("div")
+            .classed("middle", true)
 
-    m.append("div")
-      .classed("text", true)
-      .text(function (d) {
-        return d.text + ".";
-      }); //…
+        m
+            .append("div")
+            .classed("title", true)
+            .text(function (d) {
+                return d.titel;
+            })
 
-    var l = e.append("div").classed("large", true);
+        m
+            .append("div")
+            .classed("text", true)
+            .text(function (d) {
+                return d.text + ".";
+            }) //…
 
-    l.append("div")
-      .classed("title", true)
-      .text(function (d) {
-        return d.titel;
-      });
+        var l = e
+            .append("div")
+            .classed("large", true)
 
-    l.append("div")
-      .classed("text", true)
-      .html(function (d) {
-        return d.text + ".<br><br>" + d.extra;
-      });
+        l
+            .append("div")
+            .classed("title", true)
+            .text(function (d) {
+                return d.titel;
+            })
 
-    select
-      .style("transform", function (d) {
-        return "translate3d(" + parseInt(d.pos) + "px,0px,0px)";
-      })
-      .style("height", canvas.rangeBand() * scale + "px")
-      .style("width", canvas.rangeBand() * scale + "px")
-      .style("display", function (d) {
-        return (d.visible && !((timelineScale(scale * (fontSize / 2)) === "none") && (d.key % 10 !== 0))) ? "block" : "none";
-    })
+        l
+            .append("div")
+            .classed("text", true)
+            .html(function (d) {
+                return d.text + ".<br><br>" + d.extra;
+            })
 
-    select.select(".year").style("font-size", fontScaleYear(scale) + "px");
-  };
+        select
+            .style("transform", function (d) {
+                return "translate3d(" + parseInt(d.pos) + "px,0px,0px)";
+            })
+            .style("height", canvas.rangeBand() * scale + "px")
+            .style("width", canvas.rangeBand() * scale + "px")
+            .style("display", function (d) {
+                return (d.visible && !((timelineScale(scale * (fontSize / 2)) === "none") && (d.key % 10 !== 0))) ? "block" : "none";
+            })
 
-  return timeline;
+        select
+            .select(".year")
+            .style("font-size", fontScaleYear(scale) + "px")
+    }
+
+    return timeline
+
 }
